b67baf2d67ac4e5dc83954661de8176fa03837a9
yogiwisesa <yogiwisesa@gmail.com>
2019-08-19 14:37:23 +0700

    finished tutorial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
 import React from 'react';
 
-const App = props => (
-  <div>
-    <p>hello world</p>
-    <img src="/dist/images/dog.jpg"/>
-  </div>
-)
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      news: {},
+      Foo: null
+    };
+  }
+
+  componentDidMount() {
+    this.fetchApi();
+    this.loadComponent();
+  }
+
+  async fetchApi() {
+    const url =
+      'https://newsapi.org/v2/everything?q=bitcoin&from=2019-07-19&sortBy=publishedAt&apiKey=109cafd7e6a94b0cae047707712c8c25';
+
+    try {
+      const response = await fetch(url);
+      const responseParsed = await response.json();
+
+      this.setState({
+        news: responseParsed
+      });
+    } catch (err) {
+      console.log(`Request error ${err}`);
+    }
+  }
+
+  // Code splitting
+  loadComponent() {
+    import(/* webpackChunkName: 'Foo' */ './Foo').then(Foo => {
+      this.setState({
+        Foo: Foo.default
+      });
+    });
+  }
+
+  render() {
+    const { Foo } = this.state;
+
+    return (
+      <div>
+        <p> hello world </p> <img src="/dist/images/dog.jpg" /> 
+        {Foo ? <Foo/> : null}
+        <p> {JSON.stringify(this.state.news.articles)} </p>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/Foo.js
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+export default () => (
+  <div>
+    <p>I am Foo! Pleasure to meet you. </p>
+  </div>
+);
